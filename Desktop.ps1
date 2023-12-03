41c268ace6abc9186810e1bcfce2356e5cf06a5e
unknown <imahdzadeh@yahoo.com>
2023-12-03 11:03:33 -0500

    test45

---
+++ Desktop.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$Secoform = New-Object Windows.Forms.Form -Property @{
+    StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
+    Size          = New-Object Drawing.Size 800, 600
+    Text          =            $shamsiYear.ToString() + "/" + 
+                               ($shamsiMonth = $persianCalendar.GetMonth($gregorianDate)) + "/" + 
+                               ( $shamsiDay = $persianCalendar.GetDayOfMonth($gregorianDate)).ToString() + "`t`t`t`t`t`t`t`t`t`t" + "پارس طوبی تکیه" 
+    Topmost       = $true
+}
+$Secoform.AutoScale = $false
+    [void] $SecoForm.Show()
