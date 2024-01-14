80dd5ef976b54f7391d912ea2c212906a110dacf
unknown <mahdza1atenshinelsci@gmail.com>
2024-01-14 16:00:08 +0330

    011424

--- Main/OF11/ComMan/FrmSupMgmt.ps1
+++ Main/OF11/ComMan/FrmSupMgmt.ps1
@@ -1,4 +1,4 @@
-﻿# Created by Atena Mahdzadeh
+﻿# Created by Atena Mahdzadeh - FrmSupMgmt.ps1
 # Jan 12 2024
 #
 #Try
@@ -26,6 +26,17 @@ $MainTable.BackColor= ' '
 $MainTable.CellBorderStyle = "Single"
 $MainTable.AutoSize = $true
 
+#SupplyInfoTable Control
+$SupplyInfoTable = New-Object System.Windows.Forms.TableLayoutPanel
+$SupplyInfoTable.Dock = "fill"
+$SupplyInfoTable.BackColor= ' '
+$SupplyInfoTable.CellBorderStyle = "Single"
+#$SupplyInfoTable.AutoSize = $true
+$SupplyInfoTable.ColumnCount = 2
+$SupplyInfoTable.RowCount = 2
+$SupplyInfoTable.BackColor = 'green'
+
+
 #کنترل دکمه بازگشت به صفحه اصلی
 $ReturnBtn = New-Object system.Windows.Forms.Button
 $ReturnBtn.Name = $retunBtnName
@@ -35,7 +46,30 @@ $ReturnBtn.width = 100
 $ReturnBtn.height = 35
 $ReturnBtn.Font = 'Microsoft Sans Serif,10'
 $ReturnBtn.ForeColor = "#000"
-#+++++++++++++++++++++++++++++++++++++++++++++++
+
+# MainGroupboxControl                       -Created by atena jan 14 24 
+$DesktopGB = New-Object system.Windows.Forms.Groupbox
+$DesktopGB.Size = New-Object System.Drawing.Size(500,550)
+#$DesktopGB.Dock = [System.Windows.Forms.DockStyle]::Fill
+$DesktopGB.AutoSize = $true
+$DesktopGB.BackColor = ''
+
+#GroupBoxControl -                          -Created by atena jan 14 24 
+$MainGBLbl = New-Object System.Windows.Forms.label
+$MainGBLbl.Location = New-Object System.Drawing.size(520,-5)
+$MainGBLbl.Size = New-Object System.Drawing.Size(250,20) 
+$MainGBLbl.Text = "ایجــاد یا ویــرایــش اطـــلاعات تامـــیــن کنـنده" 
+$MainGBLbl.Font = 'Arial'
+$MainGBLbl.TextAlign=[System.Drawing.ContentAlignment]::BottomRight    
+                                         
+#SupportNameLbl -                           -Created by atena jan 14 24 
+$NameLbl = New-Object System.Windows.Forms.label
+#$NameLbl.Location = New-Object System.Drawing.size(195,85)
+#$NameLbl.Size = New-Object System.Drawing.Size(70,20) 
+$NameLbl.Text = "نام سازمان تامین کننده" 
+$NameLbl.TextAlign=[System.Drawing.ContentAlignment]::TopLeft
+#$NameLbl.Visible = $False
+$NameLbl.BackColor = 'red'
 
 # رویداد کلیک دکمه بازگشت به صفحه اصلی
     $ReturnBtn.Add_Click({
@@ -46,6 +80,10 @@ $ReturnBtn.ForeColor = "#000"
 
 #اضافه کردن کنترلها به فرم و جدول اصلی
 $MainTable.Controls.Add($ReturnBtn)
-$Mainform.Controls.Add($MainTable)
+$Mainform.Controls.add($DesktopGB)
+$DesktopGB.Controls.Add($MainTable)
+$DesktopGB.Controls.Add($MainGBLbl)
+$MainTable.Controls.Add($SupplyInfoTable)
+$SupplyInfoTable.Controls.Add($NameLbl)
 
 [void]$Mainform.ShowDialog()
