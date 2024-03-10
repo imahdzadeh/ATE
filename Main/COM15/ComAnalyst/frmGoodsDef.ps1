386b2e5b6fda885ee781949efeb3650db6816430
unknown <mahdza1atenshinelsci@gmail.com>
2024-03-10 21:03:25 +0330

    021220

--- Main/COM15/ComAnalyst/frmGoodsDef.ps1
+++ Main/COM15/ComAnalyst/frmGoodsDef.ps1
@@ -19,82 +19,80 @@ if($varDebugTrace -ne 0){Set-PSDebug -Trace $varDebugTrace}Else{Set-PSDebug -Tra
 
 
 $MainTbl = New-Object TableLayoutPanel
-$MainTbl.AutoSize = $true
+#$MainTbl.AutoSize = $true
 $MainTbl.CellBorderStyle = 1
-$MainTbl.ColumnCount = 1
-$MainTbl.RowCount = 6
-#-------Add other tables to form
-If (Test-Path $TablesCSV)
-    {
-        $TablesCSV = Import-Csv $TablesCSV
-        $TablesCSV | % {                     
-            New-Variable -Force -Name $_.TableName -Value (New-Object $_.obj)
-            $thisTable = Get-Variable -ValueOnly -Include $_.TableName
-            $thisTable.Name = $_.TableName
-            $thisTable.AutoSize = $_.AutoSize
-            $thisTable.ColumnCount = $_.ColumnCount
-            $thisTable.RowCount = $_.RowCount
-            $thisTable.CellBorderStyle = $_.CellBorderStyle
-            $thisTable.Height = $_.Height
-            $MainTbl.Controls.Add($thisTable)
-        }
-    }
-#---------Add controls to table
-    If (Test-Path $ControlsCSV )
-    {
-        $Controls = Import-Csv $ControlsCSV
-        $Controls | % {
-            If($_.Obj -ne "System.Windows.Forms.TableLayoutPanel"){
-                New-Variable -Force -Name $_.objName -Value (New-Object $_.obj)
-                $thisControl = Get-Variable -ValueOnly -Include $_.objName
-                $thisControl.Name = $_.objName
-            }
-            Else
-            {
-                $thisControl = Get-Variable -ValueOnly -Include $_.objName
-            }
-            $thisControl.Size = New-Object System.Drawing.Size($_.width,$_.Height)
-            If($_.ImageName -ne [System.DBNull]::Value){$thisControl.Image = funImgStreamer "$imgFol\$($_.ImageName)$imgFileExt"}     
-            If($_.ImageAlign -ne [System.DBNull]::Value){$thisControl.ImageAlign = $_.ImageAlign}
-            If($_.Padding -ne [System.DBNull]::Value){$thisControl.Padding = $_.Padding}  
-            If($_.Margin -ne [System.DBNull]::Value){$thisControl.Margin = $_.Margin}
-            If($_.Tag -ne [System.DBNull]::Value){$thisControl.Tag = $_.Tag}  
-            If($_.Appearance -ne [System.DBNull]::Value){$thisControl.Appearance = $_.Appearance}
-            If($_.FlatStyle -ne [System.DBNull]::Value){$thisControl.FlatStyle = $_.FlatStyle}
-            If($_.BackColor -ne [System.DBNull]::Value){$thisControl.BackColor = $_.BackColor}
-            If($_.Text -ne [System.DBNull]::Value){$thisControl.Text = $_.Text}
-            If($_.ForeColor -ne [System.DBNull]::Value){$thisControl.ForeColor = $_.ForeColor}
-            If($_.TextAlign -ne [System.DBNull]::Value){$thisControl.TextAlign = $_.TextAlign}
-            If($_.Anchor -ne [System.DBNull]::Value){$thisControl.Anchor = $_.Anchor}
-            If($_.functions -ne [System.DBNull]::Value)
-            {
-                $funName = $_.functions
-                New-Variable -Force -Name "$($_.objName)var" -Value ($_.functions)
-                $thisControl.Add_Click({
-#                    If(!$This.Checked){$DesktopPan.Focus()}
-                   # funDisAllShapes  $This
-                   # $ObjFunVar = Get-Variable -ValueOnly -Include "$($This.Name)$VarFileCont"
-                    #invoke-expression  $ObjFunVar
-                })                                     
-            }
-            Else
-            {
-                $thisControl.Add_Click({
-#                    If(!$This.Checked){$DesktopPan.Focus()}
-#                   funDisAllShapes  $This
-                })                  
-            }
-            $Thistbl = Get-Variable -ValueOnly -Include $_.TableName
-            $Thistbl.controls.add($thisControl)                
-        }                                    
-    }
+$MainTbl.Size = New-Object Size(1100,601)
+#$MainTbl.ColumnCount = 4
+#$MainTbl.RowCount = 3
+#$MainTbl.BackColor = 'green'
 
+$HeaderTbl = New-Object TableLayoutPanel
+#$HeaderTbl.AutoScroll = $true
+#$HeaderTbl.Controls.Add($DesktopPan)
+$HeaderTbl.CellBorderStyle = 2
+$HeaderTbl.Size = New-Object Size(1100,100)
+$HeaderTbl.ColumnCount = 3
+$HeaderTbl.RowCount = 3
+$HeaderTbl.BackColor = 'pink'
 
+$HeaderTblLbl1 = New-Object System.Windows.Forms.label
+#$HeaderTblLbl1.Location = New-Object System.Drawing.size(520,-5)
+#$HeaderTblLbl1.Size = New-Object System.Drawing.Size(250,20) 
+$HeaderTblLbl1.Text = "فرم تعریف کالا" 
+$HeaderTblLbl1.Font = 'Arial'
+$HeaderTblLbl1.TextAlign=[System.Drawing.ContentAlignment]::TopRight  
 
+$HeaderTblLbl2 = New-Object System.Windows.Forms.label
+$HeaderTblLbl2.Text = ":شناسه" 
+$HeaderTblLbl2.Font = 'Arial'
+$HeaderTblLbl2.TextAlign=[System.Drawing.ContentAlignment]::TopLeft
+
+$HeaderTblLbl3 = New-Object System.Windows.Forms.label
+#$HeaderTblLbl1.Location = New-Object System.Drawing.size(520,-5)
+#$HeaderTblLbl1.Size = New-Object System.Drawing.Size(250,20) 
+$HeaderTblLbl3.Text = "WHE-FR-001-00" 
+$HeaderTblLbl3.Font = 'Arial'
+$HeaderTblLbl3.TextAlign=[System.Drawing.ContentAlignment]::TopRight
+
+$HeaderTblDateLbl = New-Object System.Windows.Forms.label
+$HeaderTblDateLbl.Text = ":تاریخ بازنگری" 
+$HeaderTblDateLbl.Font = 'Arial'
+$HeaderTblDateLbl.TextAlign=[System.Drawing.ContentAlignment]::TopLeft
+
+$HeaderTblDateValueLbl = New-Object System.Windows.Forms.label
+$HeaderTblDateValuelbl.Text = "1402-12-04" 
+$HeaderTblDateValueLbl.Font = 'Arial'
+$HeaderTblDateValueLbl.TextAlign=[System.Drawing.ContentAlignment]::TopRight
+
+$HeaderTblRefLbl = New-Object System.Windows.Forms.label
+$HeaderTblRefLbl.Text = ":مرجع" 
+$HeaderTblRefLbl.Font = 'Arial'
+$HeaderTblRefLbl.TextAlign=[System.Drawing.ContentAlignment]::TopLeft
+
+$HeaderTblRefValueLbl = New-Object System.Windows.Forms.label
+$HeaderTblRefValuelbl.Text = "WHE-SOP-001-00" 
+$HeaderTblRefvalueLbl.Font = 'Arial'
+$HeaderTblRefValueLbl.TextAlign=[System.Drawing.ContentAlignment]::TopRight
+
+$HeaderTblNameLbl = New-Object System.Windows.Forms.label
+$HeaderTblNamelbl.Text = "شرکت مهــــــدپــویـــــــان اطلــــــس" 
+$HeaderTblNameLbl.Font = 'Arial'
+#$HeaderTblNameLbl.TextAlign=[System.Drawing.ContentAlignment]::MiddleCenter
 
 $Secoform.Controls.Add($MainTbl)
+$MainTbl.Controls.Add($HeaderTbl)
+#$HeaderTbl.Controls.Add($HeaderTblLbl1,2,2)
+$HeaderTbl.Controls.Add($HeaderTblLbl2,1,0)
+$HeaderTbl.Controls.Add($HeaderTblLbl3,0,0)
+$HeaderTbl.Controls.Add($HeaderTblDateLbl,1,1)
+$HeaderTbl.Controls.Add($HeaderTblDateValueLbl,0,1)
+$HeaderTbl.Controls.Add($HeaderTblRefLbl,1,2)
+$HeaderTbl.Controls.Add($HeaderTblRefValueLbl,0,2)
+$HeaderTbl.Controls.Add($HeaderTblNameLbl,2,0)
+$HeaderTbl.SetRowSpan($HeaderTblNameLbl,2)
+#$HeaderTbl.SetRowSpan(2)
 
-
+#$Secoform.Controls.Add($HeaderTbl)
     
 
 
